60e651709abec6196527b42faafaae258bcf0ff4
ep-code-box <nightace7@live.co.kr>
2025-08-25 17:21:05 +0900

    누락 import추가.

--- tools/subgraph_tool.py
+++ tools/subgraph_tool.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 from langgraph.graph import StateGraph, START, END
 from core.schemas import AgentState
 from typing_extensions import TypedDict
